50b77e9b885cbf27a13628c86445bba0337c4cba
Anas Baig <anasraees33@gmail.com>
2024-07-01 18:18:07 +0500

    done-done

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,14 @@
 import React from "react";
-import Navbar from "@/Components/navabr/Navbar";
-import Product from "@/Components/Product";
-import Acces from "@/Components/Acces";
+import Navbar from "@/components/navabr/Navbar";
+import Product from "@/components/Product";
+import Acces from "@/components/Acces";
 
 const page = () => {
   return (
     <div className="bg-gradient-to-r from-gray-600 via-blue-800 to-gray-400 w-full h-full   ">
-      /* <Navbar />
-      <Product /> */
-<Acces/>
+      <Navbar/>
+      <Product/>
+      <Acces/>
     </div>
   );
 };
