3d714a267fd4499bf70602c598ab273a689fc75f
Anas Baig <anasraees33@gmail.com>
2024-07-01 18:24:39 +0500

    done

--- src/app/components/Product.jsx
+++ src/app/components/Product.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Card from "./Card";
-
 function Product() {
   const data = [
     {
--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,12 @@
 "use client"
 import React from "react";
-import Navbar from "@/components/navabr/Navbar";
-import Product from "@/components/Product";
-import Acces from "@/components/Acces";
-
+import Navbar from "./components/navabr/Navbar";
+import Product from "./components/Product";
 const page = () => {
   return (
     <div className="bg-gradient-to-r from-gray-600 via-blue-800 to-gray-400 w-full h-full   ">
       <Navbar/>
       <Product/>
-      <Acces/>
     </div>
   );
 };
