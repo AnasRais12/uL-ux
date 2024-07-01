4affdc0e2f22aa7308677f28bf312856ae911dc6
Anas Baig <anasraees33@gmail.com>
2024-07-01 13:49:45 +0500

    Last Change

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "@/Components/navabr/Navbar";
 import Product from "@/Components/Product";
+import Acces from "@/Components/Acces";
 
 const page = () => {
   return (
     <div className="bg-gradient-to-r from-gray-600 via-blue-800 to-gray-400 w-full h-full   ">
       /* <Navbar />
       <Product /> */
+<Acces/>
     </div>
   );
 };
