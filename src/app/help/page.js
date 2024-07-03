20b4f5537f38699d9ade6cbec801b294e6bd9d63
Anas Baig <anasraees33@gmail.com>
2024-07-03 22:19:01 +0500

    page-done

--- src/app/help/page.js
+++ src/app/help/page.js
@@ -3,7 +3,11 @@ import React from 'react'
 const Help = () => {
   return (
     <>
-    <div className='w-full h-full bg-red-500'>Help</div>
+    <div className='w-full h-[40vh] font-bold flex justify-center items-center text-white bg-gradient-to-r from-green-950 via-green-500  to-black text-[30px] '>
+      <h1>How Can We Help You?</h1>
+
+
+    </div>
     </>
   )
 }
